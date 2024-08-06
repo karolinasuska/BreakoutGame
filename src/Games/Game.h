5571f2ca9ee3ab1131985dd7625aed82cb3dc68e
Karolina Suska <karolina1101suska@gmail.com>
2024-08-06 13:49:36 +0200

    feat: add file command loader

---
+++ src/Games/Game.h
@@ -0,0 +1,31 @@
+/*
+ * Game.h
+ *
+ *  Created on: Aug 6, 2024
+ *      Author: karolina
+ */
+
+#ifndef GAMES_GAME_H_
+#define GAMES_GAME_H_
+
+#include <string>
+#include <stdint.h>
+
+class GameController;
+class Screen;
+
+class Game
+{
+public:
+
+	virtual ~Game(){}
+	virtual void Init(GameController& controller) = 0;
+	virtual void Update(uint32_t dt) = 0;
+	virtual void Draw(Screen& screen) = 0;
+	virtual std::string GetSceneName() const = 0;
+
+};
+
+
+
+#endif /* GAMES_GAME_H_ */
---
+++ src/Scenes/GameScene.cpp
@@ -0,0 +1,36 @@
+/*
+ * GameScene.cpp
+ *
+ *  Created on: Aug 6, 2024
+ *      Author: karolina
+ */
+
+#include "GameScene.h"
+
+GameScene::GameScene(std::unique_ptr<Game> optrGame): mGame(std::move(optrGame))
+{
+
+}
+
+void GameScene::Init()
+{
+	mGame -> Init(mGameController);
+}
+
+void GameScene::Update(uint32_t dt)
+{
+	mGame -> Update(dt);
+}
+
+void GameScene::Draw(Screen& screen)
+{
+	mGame -> Draw(screen);
+}
+
+const std::string& GameScene::GetSceneName() const
+{
+	static std::string name = "";
+	return name;
+}
+
+
